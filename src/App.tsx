ad01f8f2286bf766a5189f59b4e05dbd8d7f9493
João Gabriel <joaogkvalho@gmail.com>
2021-06-22 10:43:45 -0300

    auth page initiated

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import { Button } from './components/Button'
+import { Home } from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-    </>
-  );
+    <Home />
+  )
 }
 
 export default App;
