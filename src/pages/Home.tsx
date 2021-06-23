5b98ec7481cacbb1e6b8ef65ebed163bde595de8
João Gabriel <joaogkvalho@gmail.com>
2021-06-23 14:47:36 -0300

    join and create room finished

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { useHistory } from 'react-router-dom'
 import { useAuth } from '../hooks/useAuth'
+import { FormEvent, useState } from 'react'
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
@@ -7,6 +8,8 @@ import googleIconImg from '../assets/images/google-icon.svg'
 
 import '../styles/auth.scss'
 import { Button } from '../components/Button'
+import { database } from '../services/firebase'
+
 
 
 
@@ -15,6 +18,7 @@ import { Button } from '../components/Button'
 export function Home() {
     const history = useHistory()
     const { signInWithGoogle, user } = useAuth()
+    const [roomCode, setRoomCode] = useState('')
 
     async function handleCreateRoom() {
         if (!user) {
@@ -22,6 +26,23 @@ export function Home() {
         }
 
         history.push('rooms/new')
+    }
+
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault()
+
+        if (roomCode.trim() === '') {
+            return
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get()
+
+        if (!roomRef.exists()) {
+            alert('Room does not exist.')
+            return
+        }
+
+        history.push(`rooms/${roomCode}`)
     }
 
     return (
@@ -39,10 +60,12 @@ export function Home() {
                         Crie sua sala com o Google
                     </button>
                     <div className="separator">ou entre em uma sala</div>
-                    <form>
+                    <form onSubmit={handleJoinRoom}>
                         <input
                             type="text"
                             placeholder="Digite o código da sala"
+                            onChange={event => setRoomCode(event.target.value)}
+                            value={roomCode}
 
                         />
                         <Button type="submit">
